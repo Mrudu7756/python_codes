7301522eb4980eb8dc2fb0b8165f1d4d73d67152
Mrudula Rajendra Tandel <163021229+Mrudu7756@users.noreply.github.com>
2026-02-21 15:07:47 +0530

    Add files via upload

---
+++ list_methods.py
@@ -0,0 +1,15 @@
+list1 = ["Mrudula","Purple",7,"blue",False]
+list2 = [5,8,3,8,2,9,2,6]
+
+list2.sort()
+print(list2)
+list2.reverse()
+print(list2)
+list2.append(12)
+print(list2)
+list2.insert(0,1)
+print(list2)
+list1.pop(4)
+print(list1)
+list2.remove(2)
+print(list2)
---
+++ tuple_methods.py
@@ -0,0 +1,6 @@
+a = (1,7,3,8,4)
+
+print(a.count(3))
+print(a.index(3))
+print(len(a))
+print(type(a))
